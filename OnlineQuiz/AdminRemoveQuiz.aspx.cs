53b46f94ef9d1e0ff07393eba0def02ddba8f12a
ROHAN SOLANKI <rrohanvsolanki@outlook.com>
2025-11-13 15:19:39 +0530

    remove quiz page done

--- OnlineQuiz/AdminRemoveQuiz.aspx.cs
+++ OnlineQuiz/AdminRemoveQuiz.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +13,44 @@ namespace OnlineQuiz
 {
     public partial class AdminRemoveQuiz : System.Web.UI.Page
     {
+        string s = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+        string fnm;
         protected void Page_Load(object sender, EventArgs e)
         {
+            getcon();
+            fill_grid();
+        }
+        void getcon()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+        void fill_grid()
+        {
+            getcon();
+            da = new SqlDataAdapter("Select * from add_course", con);
+            ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
 
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "cmd_dlt")
+            {
+                getcon();
+                int id = Convert.ToInt32(e.CommandArgument);
+                ViewState["id"] = id;
+                cmd = new SqlCommand("delete from add_course where Id = '" + ViewState["id"] + "'", con);
+                cmd.ExecuteNonQuery();
+                fill_grid();
+            }
         }
     }
 }
